cd7a5dce11056f6cfd5029330beef8825ebfdb9f
nomuffn <robin.rs80@gmail.com>
2022-04-12 01:10:31 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
             '@': fileURLToPath(new URL('./src', import.meta.url)),
         },
     },
-    // css: { 
+    // css: {  
     //     preprocessorOptions: {
     //         scss: {
     //             additionalData: `
